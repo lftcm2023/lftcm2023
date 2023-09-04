95c38470e058cd28cc873bd6ae57378a6bd05fdd
Alexander Bentkamp <bentkamp@gmail.com>
2023-09-04 09:35:17 +0200

    delete all but first session

--- LftCM.lean
+++ LftCM.lean
@@ -1,41 +1,41 @@
 import LftCM.C01_Basics.S01_Calculating
-import LftCM.C01_Basics.S02_Overview
-import LftCM.C01_Basics.S03_Proving_Identities_in_Algebraic_Structures
-import LftCM.C01_Basics.S04_More_on_Order_and_Divisibility
-import LftCM.C01_Basics.S05_Proving_Facts_about_Algebraic_Structures
-import LftCM.C01_Basics.S06_Using_Theorems_and_Lemmas
-import LftCM.C02_Logic.Logic
-import LftCM.C02_Logic.S01_Implication_and_the_Universal_Quantifier
-import LftCM.C02_Logic.S02_The_Existential_Quantifier
-import LftCM.C02_Logic.S03_Negation
-import LftCM.C02_Logic.S04_Conjunction_and_Iff
-import LftCM.C02_Logic.S05_Disjunction
-import LftCM.C02_Logic.S06_Sequences_and_Convergence
-import LftCM.C03_Sets_and_Functions.S01_Sets
-import LftCM.C03_Sets_and_Functions.S02_Functions
-import LftCM.C03_Sets_and_Functions.S03_The_Schroeder_Bernstein_Theorem
-import LftCM.C04_Algebra_Tactics.S01BasicsOfPolyrith
-import LftCM.C04_Algebra_Tactics.S02UsingPolyrith
-import LftCM.C04_Algebra_Tactics.S03NonsingularityOfCurves
-import LftCM.C04_Algebra_Tactics.S04CombiningTactics
-import LftCM.C05_Structures_And_Classes.S01_Structures
-import LftCM.C05_Structures_And_Classes.S02_Algebraic_Structures
-import LftCM.C05_Structures_And_Classes.S03_Building_the_Gaussian_Integers
-import LftCM.C07_Algebraic_Hierarchy.S01_Basics
-import LftCM.C07_Algebraic_Hierarchy.S02_Morphisms
-import LftCM.C07_Algebraic_Hierarchy.S03_Subobjects
-import LftCM.C08_Topology.S01_Filters
-import LftCM.C08_Topology.S02_Metric_Spaces
-import LftCM.C08_Topology.S03_Topological_Spaces
-import LftCM.C09A_Analysis.S01_Elementary_Differential_Calculus
-import LftCM.C09A_Analysis.S02_Differential_Calculus_in_Normed_Spaces
-import LftCM.C09A_Analysis.S03_Integration
-import LftCM.C09A_Analysis.S04_Measure_Theory
-import LftCM.C09A_Analysis.S05_Elementary_Integration
-import LftCM.C09B_Combinatorics.Combinatorics
-import LftCM.C10_Category_Theory.CategoryTheory
-import LftCM.C11_Algebraic_Geometry.Algebraic_Geometry
-import LftCM.C12A_Differential_Geometry.Differential_Geometry
-import LftCM.C12B_Number_Theory.S01_Irrational_Roots
-import LftCM.C12B_Number_Theory.S02_Induction_and_Recursion
-import LftCM.C12B_Number_Theory.S03_Infinitely_Many_Primes
+-- import LftCM.C01_Basics.S02_Overview
+-- import LftCM.C01_Basics.S03_Proving_Identities_in_Algebraic_Structures
+-- import LftCM.C01_Basics.S04_More_on_Order_and_Divisibility
+-- import LftCM.C01_Basics.S05_Proving_Facts_about_Algebraic_Structures
+-- import LftCM.C01_Basics.S06_Using_Theorems_and_Lemmas
+-- import LftCM.C02_Logic.Logic
+-- import LftCM.C02_Logic.S01_Implication_and_the_Universal_Quantifier
+-- import LftCM.C02_Logic.S02_The_Existential_Quantifier
+-- import LftCM.C02_Logic.S03_Negation
+-- import LftCM.C02_Logic.S04_Conjunction_and_Iff
+-- import LftCM.C02_Logic.S05_Disjunction
+-- import LftCM.C02_Logic.S06_Sequences_and_Convergence
+-- import LftCM.C03_Sets_and_Functions.S01_Sets
+-- import LftCM.C03_Sets_and_Functions.S02_Functions
+-- import LftCM.C03_Sets_and_Functions.S03_The_Schroeder_Bernstein_Theorem
+-- import LftCM.C04_Algebra_Tactics.S01BasicsOfPolyrith
+-- import LftCM.C04_Algebra_Tactics.S02UsingPolyrith
+-- import LftCM.C04_Algebra_Tactics.S03NonsingularityOfCurves
+-- import LftCM.C04_Algebra_Tactics.S04CombiningTactics
+-- import LftCM.C05_Structures_And_Classes.S01_Structures
+-- import LftCM.C05_Structures_And_Classes.S02_Algebraic_Structures
+-- import LftCM.C05_Structures_And_Classes.S03_Building_the_Gaussian_Integers
+-- import LftCM.C07_Algebraic_Hierarchy.S01_Basics
+-- import LftCM.C07_Algebraic_Hierarchy.S02_Morphisms
+-- import LftCM.C07_Algebraic_Hierarchy.S03_Subobjects
+-- import LftCM.C08_Topology.S01_Filters
+-- import LftCM.C08_Topology.S02_Metric_Spaces
+-- import LftCM.C08_Topology.S03_Topological_Spaces
+-- import LftCM.C09A_Analysis.S01_Elementary_Differential_Calculus
+-- import LftCM.C09A_Analysis.S02_Differential_Calculus_in_Normed_Spaces
+-- import LftCM.C09A_Analysis.S03_Integration
+-- import LftCM.C09A_Analysis.S04_Measure_Theory
+-- import LftCM.C09A_Analysis.S05_Elementary_Integration
+-- import LftCM.C09B_Combinatorics.Combinatorics
+-- import LftCM.C10_Category_Theory.CategoryTheory
+-- import LftCM.C11_Algebraic_Geometry.Algebraic_Geometry
+-- import LftCM.C12A_Differential_Geometry.Differential_Geometry
+-- import LftCM.C12B_Number_Theory.S01_Irrational_Roots
+-- import LftCM.C12B_Number_Theory.S02_Induction_and_Recursion
+-- import LftCM.C12B_Number_Theory.S03_Infinitely_Many_Primes
